a1eb05487ea0b0a23109564a4a0ab7d6cb97afbb
monir <test@google.com>
2019-09-25 17:47:52 +0600

    Division by 0 problem solve

--- src/classes/Division.php
+++ src/classes/Division.php
@@ -4,6 +4,7 @@ namespace src\classes;
 
 class Division implements OparationInterface{
     function run($number,$result){
-        return $result / $number;
+        
+        return  $number==0? 0: $result / $number;
     }
 }
